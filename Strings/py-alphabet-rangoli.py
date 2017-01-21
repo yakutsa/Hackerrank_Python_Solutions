3335426ecbad2b5a886aa4a3deecbfca32dca9d8
Savas Yakut <savas.yakut@gmail.com>
2017-01-21 18:26:08 +0100

    string

---
+++ Strings/py-alphabet-rangoli.py
@@ -0,0 +1,10 @@
+alpha = "abcdefghijklmnopqrstuvwxyz"
+
+n = int(input())
+L = []
+for i in range(n):
+    s = "-".join(alpha[i:n])
+    L.append((s[::-1]+s[1:]).center(4*n-3, "-"))
+
+print L
+#print('\n'.join(L[:0:-1]+L))
